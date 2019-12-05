74fa45a339cd39ad2349681a2f094e00cb5790ed
Felix <felix1.stoecker@st.oth-regensburg.de>
2019-12-05 09:24:59 +0100

    started with simulator maybe it works? (Testing now)

--- io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/Source.java
+++ io.openems.edge.simulator/src/io/openems/edge/simulator/datasource/csv/Source.java
@@ -5,8 +5,8 @@ public enum Source {
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_STANDARD_LOAD_PROFILE("h0-summer-weekday-standard-load-profile.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION("h0-summer-weekday-pv-production.csv"), //
 	H0_HOUSEHOLD_SUMMER_WEEKDAY_NON_REGULATED_CONSUMPTION("h0-summer-weekday-non-regulated-consumption.csv"), //
-	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv");
-
+	H0_HOUSEHOLD_SUMMER_WEEKDAY_PV_PRODUCTION2("h0-summer-weekday-pv-production2.csv"),
+	TEMPERATURE_STANDARD_LOAD_PROFILE("temperature-standard-load-profile.csv");
 	public final String filename;
 
 	private Source(String filename) {
