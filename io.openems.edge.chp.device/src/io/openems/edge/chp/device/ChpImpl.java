1d3e833f3acece721fc943cdb2b5726de0d62962
Felix <felix1.stoecker@st.oth-regensburg.de>
2020-01-13 12:13:37 +0100

    Started with implementing Chp channels
    TODO define ModbusProtocol and  debug log

--- io.openems.edge.chp.device/src/io/openems/edge/chp/device/ChpImpl.java
+++ io.openems.edge.chp.device/src/io/openems/edge/chp/device/ChpImpl.java
@@ -1,10 +1,15 @@
 package io.openems.edge.chp.device;
 
+import io.openems.common.channel.Unit;
 import io.openems.common.exceptions.OpenemsError;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.task.FC16WriteRegistersTask;
 import io.openems.edge.chp.device.api.PowerLevel;
 import io.openems.edge.chp.device.task.ChpTaskImpl;
 import io.openems.edge.chp.module.api.ChpModule;
-import io.openems.edge.common.component.AbstractOpenemsComponent;
+import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.component.ComponentManager;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.i2c.mcp.api.Mcp;
@@ -21,7 +26,7 @@ import org.osgi.service.metatype.annotations.Designate;
 @Component(name = "Chp",
         configurationPolicy = ConfigurationPolicy.REQUIRE,
         immediate = true)
-public class ChpImpl extends AbstractOpenemsComponent implements OpenemsComponent, PowerLevel {
+public class ChpImpl extends AbstractOpenemsModbusComponent implements OpenemsComponent, PowerLevel {
     private Mcp mcp;
     private ChpType chpType;
     @Reference
@@ -115,6 +120,7 @@ public class ChpImpl extends AbstractOpenemsComponent implements OpenemsComponen
 
     @Override
     public String debugLog() {
+        String chpInformations = getInformations();
         if (this.getPowerLevelChannel().getNextValue().get() != null) {
             if (chpType != null) {
                 return "Chp: " + this.chpType.getName() + "is at " + this.getPowerLevelChannel().getNextValue().get();
@@ -124,5 +130,76 @@ public class ChpImpl extends AbstractOpenemsComponent implements OpenemsComponen
         }
         return "Percentage Level at 0";
     }
+
+    private String getInformations() {
+        return null;
+    }
+
+    public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+
+        MODUS(Doc.of(OpenemsType.INTEGER)),
+        STATUS(Doc.of(OpenemsType.INTEGER)),
+        MODE(Doc.of(OpenemsType.INTEGER)),
+        SETPOINT_OPERATION_MODE(Doc.of(OpenemsType.INTEGER).unit(Unit.PERCENT)),
+        ERROR_BITS_1(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_2(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_3(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_4(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_5(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_6(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_7(Doc.of(OpenemsType.INTEGER)),
+        ERROR_BITS_8(Doc.of(OpenemsType.INTEGER)),
+        OPERATING_HOURS(Doc.of(OpenemsType.INTEGER).unit(Unit.HOUR)),
+        OPERATING_MINUTES(Doc.of(OpenemsType.INTEGER).unit(Unit.MINUTE)),
+        START_COUNTER(Doc.of(OpenemsType.INTEGER)),
+        MAINTENANCE_INTERVAL(Doc.of(OpenemsType.INTEGER).unit(Unit.HOUR)),
+        MODULE_LOCK(Doc.of(OpenemsType.INTEGER).unit(Unit.HOUR)),
+        WARNING_TIME(Doc.of(OpenemsType.INTEGER).unit(Unit.HOUR)),
+        NEXT_MAINTENANCE(Doc.of(OpenemsType.INTEGER).unit(Unit.HOUR)),
+        EXHAUST_A(Doc.of(OpenemsType.INTEGER).unit(Unit.DEGREE_CELSIUS)),
+        EXHAUST_B(Doc.of(OpenemsType.INTEGER).unit(Unit.DEGREE_CELSIUS)),
+        EXHAUST_C(Doc.of(OpenemsType.INTEGER).unit(Unit.DEGREE_CELSIUS)),
+        EXHAUST_D(Doc.of(OpenemsType.INTEGER).unit(Unit.DEGREE_CELSIUS)),
+        BATTERY_VOLTAGE(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        OIL_PRESSURE(Doc.of(OpenemsType.INTEGER).unit(Unit.BAR)),
+        LAMBDA_PROBE_Voltage(Doc.of(OpenemsType.INTEGER).unit(Unit.MILLIVOLT)),
+        ROTATION_PER_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.ROTATION_PER_MINUTE)),
+        TEMPERATURE_CONTROLLER(Doc.of(OpenemsType.INTEGER).unit(Unit.DEGREE_CELSIUS)),
+        TEMPERATURE_CLEARANCE(Doc.of(OpenemsType.INTEGER).unit(Unit.DEGREE_CELSIUS)),
+        SUPPLY_VOLTAGE_L1(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        SUPPLY_VOLTAGE_L2(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        SUPPLY_VOLTAGE_L3(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        GENERATOR_VOLTAGE_L1(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        GENERATOR_VOLTAGE_L2(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        GENERATOR_VOLTAGE_L3(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        GENERATOR_ELECTRICITY_L1(Doc.of(OpenemsType.INTEGER).unit(Unit.AMPERE)),
+        GENERATOR_ELECTRICITY_L2(Doc.of(OpenemsType.INTEGER).unit(Unit.AMPERE)),
+        GENERATOR_ELECTRICITY_L3(Doc.of(OpenemsType.INTEGER).unit(Unit.AMPERE)),
+        SUPPLY_VOLTAGE_TOTAL(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        GENERATOR_VOLTAGE_TOTAL(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT)),
+        ENGINE_PERFORMANCE(Doc.of(OpenemsType.INTEGER).unit(Unit.KILOWATT)),
+        SUPPLY_FREQUENCY(Doc.of(OpenemsType.DOUBLE).unit(Unit.HERTZ)),
+        GENERATOR_FREQUENCY(Doc.of(OpenemsType.DOUBLE).unit(Unit.HERTZ)),
+        ACTIVE_POWER_FACTOR(Doc.of(OpenemsType.INTEGER)),
+        RESERVE(Doc.of(OpenemsType.INTEGER).unit(Unit.KILOWATT_HOURS));
+
+
+        private final Doc doc;
+
+        private ChannelId(Doc doc) {
+            this.doc = doc;
+        }
+
+        public Doc doc() {
+            return this.doc;
+        }
+    }
+
+    @Override
+    protected ModbusProtocol defineModbusProtocol() {
+        return new ModbusProtocol(this,
+                new FC16WriteRegistersTask(0xFA0))
+    }
+
 
 }
